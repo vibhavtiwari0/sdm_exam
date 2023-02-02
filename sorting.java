c64d68b34b93ba86214dd1c17278a718f19ed583
Vibhav Tiwari <vibhavtiwari1@gmail.com>
2023-02-02 15:33:38 +0530

    first commit

---
+++ sorting.java
@@ -0,0 +1,27 @@
+public class  Sorting{
+    public void bubblesort(){
+        int n=arr.lenght;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i;j++)
+            {
+if (arr[j]>arr[j+1]{
+    int temp =arr[j];
+    arr[j]=arr[j+1];
+    arr[j+1]=temp
+})
+            }
+        }
+    }
+    void printArray(){
+        int n= arr.lenght;
+        for(int i =0;i<n;++i){
+            System.out.print(arr[i] +" ")
+        }
+    }
+    public static void main(String[]args) {
+        Sorting s= new Sorting();
+        
+    }
+}
+    
